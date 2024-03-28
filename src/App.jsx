8282048565a0873c8306a19ba4fb50683e1c160c
MarcoBasileDev <marco.basile1996@gmail.com>
2024-03-28 11:27:03 +0100

    add states into app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
+import { useState } from "react";
+
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectSidebar from "./components/ProgectSidebar";
 
 function App() {
+  const [projectState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleStartAddingProject() {
+    setProjectState((prevState) => {
+      return { ...prevState, selectedProjectId: null };
+    });
+  }
+
+  let content;
+
+  if (projectState.selectedProjectId === null) {
+    content = <NewProject />;
+  } else if (projectState.selectedProjectId === undefined) {
+    content = (
+      <NoProjectSelected onStartAddProject={handleStartAddingProject} />
+    );
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectSidebar />
-      <NoProjectSelected />
+      <ProjectSidebar onStartAddProject={handleStartAddingProject} />
+      {content}
     </main>
   );
 }
